b2f5212d03a060ebe3151ae109737f61044df313
Anshul <anshul.swarnkar1990@gmail.com>
2021-06-19 15:34:03 +0530

    Changes for open browser in normal mode

--- IncognitoBrowserOpen.py
+++ IncognitoBrowserOpen.py
@@ -1,11 +1,14 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
-chrome_options=webdriver.ChromeOptions()
-chrome_options.add_argument("--incognito")
+#chrome_options=webdriver.ChromeOptions()
+#chrome_options.add_argument("--incognito")
 
 #driver=webdriver.Chrome(ChromeDriverManager().install(),options=chrome_options)
 
-driver=webdriver.Chrome(executable_path="C:\\Drivers\\chromedriver_win32 (1)\\chromedriver.exe",chrome_options=chrome_options)
-
+driver= webdriver.Chrome(ChromeDriverManager().install())
 driver.get('https://www.google.com')
+driver.maximize_window()
+
+#driver=webdriver.Chrome(executable_path="C:\\Drivers\\chromedriver_win32 (1)\\chromedriver.exe",chrome_options=chrome_options)
+
